4f9d979dd9ee8f23223022986c8d2844e3a19e52
João G. C. Melo <joao.melo@ladetec.iq.ufrj.br>
2019-04-18 12:35:28 -0300

    merge

--- paginas/NavBar.blade.php
+++ paginas/NavBar.blade.php
@@ -168,7 +168,7 @@ else
                                         <font color="#A4A4A4">SUPORTE</font>
                                     </div>
                                     <div>
-                                        <a href="/suporte/chamado/create" onclick=abrirIFrame("/suporte/chamado/create") id="rota" data-target="#modal" data-toggle="modal">ABRIR CHAMADO</a><br><br>
+                                        <a href="/suporte/chamados/create" onclick=abrirIFrame("/suporte/chamados/create") id="rota" data-target="#modal" data-toggle="modal">ABRIR CHAMADO</a><br><br>
                                     </div>
                                     <div style="background-color:#E6E6E6">
                                         <font color="#A4A4A4">VISUALIZAR CHAMADOS</font>
@@ -188,8 +188,7 @@ else
                                         <font color="#A4A4A4">ÁREA ADMINISTRATIVA</font>
                                     </div>
                                     <div>
-                                        <a href="/suporte/categoria/create">CATEGORIAS</a> <br>
-                                        <a href="/suporte/subcategoria/create">SUBCATEGORIAS E TEMPLATES </a>
+                                        <a href="/suporte/categorias">CATEGORIAS</a> <br>
                                     </div>
                                 </div>
                             ';
