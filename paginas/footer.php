7e737e2c55a04ecd703dae9cc5a4e67d9d830530
Tiago Ribeiro <tiago@ladetec.iq.ufrj.br>
2019-04-24 16:20:17 -0300

    correção env footer

--- paginas/footer.php
+++ paginas/footer.php
@@ -1,5 +1,5 @@
 <script src="<?php echo $url; ?>/<?php echo $push; ?>dependencias/css_js/bootstrap/4.2.1/jquery-3.3.1.slim.min.js"></script>
-<script src="<?php echo $url; ?>/<?php echo $push; ?>css_js/bootstrap/4.2.3/bootstrap.min.js"></script>
+<script src="<?php echo $url; ?>/<?php echo $push; ?>dependencias/css_js/bootstrap/4.2.3/bootstrap.min.js"></script>
 
 </body>
 
