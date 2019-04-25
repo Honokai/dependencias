4260869966abc2cf39603b4cbc941326ea5dad43
Michel Alves <michel.alves@ladetec.iq.ufrj.br>
2019-04-25 17:31:13 -0300

    commit para deixar navbar como estava

--- paginas/NavBar.blade.php
+++ paginas/NavBar.blade.php
@@ -169,19 +169,19 @@ else
                                         <font color="#A4A4A4">SUPORTE</font>
                                     </div>
                                     <div>
-                                        <a href="/suporte/chamado/create" onclick=abrirIFrame("/suporte/chamado/create") id="rota" data-target="#modal" data-toggle="modal">NOVO CHAMADO</a><br><br>
+                                        <a href="/suporte/chamados/create" onclick=abrirIFrame("/suporte/chamados/create") id="rota" data-target="#modal" data-toggle="modal">NOVO CHAMADO</a><br><br>
                                     </div>
                                     <div style="background-color:#E6E6E6">
                                         <font color="#A4A4A4">VISUALIZAR CHAMADOS</font>
                                     </div>
                                     <div>
-                                        <a href="/suporte/chamado/CALIBRACAO">CALIBRAÇÃO</a><br>
-                                        <a href="/suporte/chamado/COMPRAS">COMPRAS</a><br>
-                                        <a href="/suporte/chamado/MANUTENCAO">MANUTENÇÃO</a><br>
-                                        <a href="/suporte/chamado/QUALIDADE">QUALIDADE</a><br>
-                                        <a href="/suporte/chamado/SECRETARIA">SECRETARIA</a><br>
-                                        <a href="/suporte/chamado/SPET">SPET</a><br>
-                                        <a href="/suporte/chamado/TI">TI</a>
+                                        <a href="/suporte/chamados/CALIBRACAO">CALIBRAÇÃO</a><br>
+                                        <a href="/suporte/chamados/COMPRAS">COMPRAS</a><br>
+                                        <a href="/suporte/chamados/MANUTENCAO">MANUTENÇÃO</a><br>
+                                        <a href="/suporte/chamados/QUALIDADE">QUALIDADE</a><br>
+                                        <a href="/suporte/chamados/SECRETARIA">SECRETARIA</a><br>
+                                        <a href="/suporte/chamados/SPET">SPET</a><br>
+                                        <a href="/suporte/chamados/TI">TI</a>
                                     </div>
                                 </div>
                                 <div id="DivMenuPrincipal">
