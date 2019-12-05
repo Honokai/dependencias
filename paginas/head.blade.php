7fb526a4d19f3abc6f52b69ed0196dddd2607997
Tiago Ribeiro <tiago@ladetec.iq.ufrj.br>
2019-12-05 13:43:44 -0300

    Removido variaveis de ambiente e inserido pegar url pelo php, inserido biblioteca js para bootstrap e menu para rh

--- paginas/head.blade.php
+++ paginas/head.blade.php
@@ -22,14 +22,14 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Sistemas Ladetec</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>/<?php echo $push; ?>dependencias/css_js/bootstrap/4.2.1/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo $url; ?>/<?php echo $push; ?>dependencias/css_js/personalizacoes/css.php">
-    <link rel="stylesheet" href="<?php echo $url; ?>/<?php echo $push; ?>dependencias/css_js/fontawesome/5.11.2/css/all.css">
-    <script src="<?php echo $url; ?>/<?php echo $push; ?>dependencias/css_js/popper/1.14.7/popper.js"></script>
-    <script src="<?php echo $url; ?>/<?php echo $push; ?>dependencias/css_js/ajax/3.3.1/jquery.js"></script>
-    <link href="<?php echo $url; ?>/<?php echo $push; ?>dependencias/css_js/select2/4.0.11/select2.min.css" rel="stylesheet" />
-    <script src="<?php echo $url; ?>/<?php echo $push; ?>dependencias/css_js/select2/4.0.11/select2.min.js"></script>
-    <script src="<?php echo $url; ?>/<?php echo $push; ?>dependencias/css_js/mask/1.14.10/jquery.mask.min.js"></script>
-
+    <link rel="stylesheet" type="text/css" href="https://<?php echo $_SERVER['HTTP_HOST']; ?>/dependencias/css_js/bootstrap/4.2.1/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://<?php echo $_SERVER['HTTP_HOST']; ?>/dependencias/css_js/personalizacoes/css.php">
+    <link rel="stylesheet" href="https://<?php echo $_SERVER['HTTP_HOST']; ?>/dependencias/css_js/fontawesome/5.11.2/css/all.css">
+    <script src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/dependencias/css_js/popper/1.14.7/popper.js"></script>
+    <script src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/dependencias/css_js/ajax/3.3.1/jquery.js"></script>
+    <link href="https://<?php echo $_SERVER['HTTP_HOST']; ?>/dependencias/css_js/select2/4.0.11/select2.min.css" rel="stylesheet" />
+    <script src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/dependencias/css_js/select2/4.0.11/select2.min.js"></script>
+    <script src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/dependencias/css_js/mask/1.14.10/jquery.mask.min.js"></script>
+    <script src="https://<?php echo $_SERVER['HTTP_HOST']; ?>/dependencias/css_js/bootstrap/bootstrap.js"></script>
 </head>
 <body>
