f4d831cd1031418c253bccd674a44f01eacee321
Emerson F. Fernandes <eff@iq.ufrj.br>
2020-10-29 14:02:42 -0300

    service provider

---
+++ DependenciaServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/css_js' => public_path('dependencias/css_js'),
+        ], 'assets');
+
+        $this->publishes([
+            __DIR__.'/images' => public_path('dependencias/images'),
+        ], 'assets');
+
+        $this->publishes([
+            __DIR__.'/paginas' => resource_path('views/dependencias'),
+        ]);
+    }
+    
+}
